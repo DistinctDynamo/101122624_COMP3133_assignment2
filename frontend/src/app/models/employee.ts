06ee49bdcd3c3b68ed5c1e76b14679f904ba95ee
DistinctDynamo <nguyensteven578@gmail.com>
2026-04-13 21:54:26 -0400

    created components

---
+++ frontend/src/app/models/employee.ts
@@ -0,0 +1,9 @@
+export default interface Employee{
+    first_name: string;
+    last_name: string;
+    email: string;
+    gender: string;
+    designation: string;
+    salary: number;
+    department: string;
+}
