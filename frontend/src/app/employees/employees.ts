493d5fa472031d271b07a4abc65bdef579e0f9a0
DistinctDynamo <nguyensteven578@gmail.com>
2026-04-15 21:59:15 -0400

    writing code related to employees

--- frontend/src/app/employees/employees.ts
+++ frontend/src/app/employees/employees.ts
@@ -1,9 +1,105 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../services/employee-service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-employees',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule, RouterLink],
   templateUrl: './employees.html',
   styleUrl: './employees.css',
 })
-export class Employees {}
+export class Employees implements OnInit{
+  employees: any[] = []
+
+  isUpdating = false;
+  selectedEmployeeId: string | null = null;
+  error: any;
+
+  private readonly employeeService=inject(EmployeeService)
+
+  employeeForm = new FormGroup({
+    first_name: new FormControl('', Validators.required),
+    last_name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    gender: new FormControl(''),
+    designation: new FormControl('',Validators.required),
+    salary: new FormControl(0,Validators.required),
+    department: new FormControl('',Validators.required)
+  });
+
+
+  ngOnInit(): void {
+      this.loadEmployees()
+  }
+
+  onSubmit() {
+    if (this.employeeForm.invalid) return;
+
+    if (this.isUpdating) {
+      this.updateEmployee();
+    } else {
+      this.addEmployee();
+    }
+  }
+
+  loadEmployees(){
+     this.employeeService.getEmployees().subscribe({
+      next: (data) => {
+        console.log('Employees:', data);
+        this.employees = data;
+      },
+      error: (err) => {
+        this.error = err;
+      },
+    });
+  }
+
+   addEmployee() {
+    const employee = this.employeeForm.value;
+
+    this.employeeService.addEmployee(employee).subscribe(() => {
+      this.loadEmployees();
+      this.resetForm();
+    });
+  }
+
+   updateEmployee() {
+    if (!this.selectedEmployeeId) return;
+
+    this.employeeService
+      .updateEmployee(this.selectedEmployeeId, this.employeeForm.value)
+      .subscribe(() => {
+        this.loadEmployees();
+        this.resetForm();
+      });
+  }
+
+  deleteEmployee(id: string) {
+    if (!confirm('Are you sure you want to delete?')) return;
+    this.employeeService.deleteEmployee(id).subscribe(() => {
+      this.loadEmployees();
+    });
+  }
+
+  selectEmployee(employee: any) {
+    this.isUpdating = true;
+    this.selectedEmployeeId = employee._id;
+
+    this.employeeForm.patchValue({
+      first_name: employee.first_name,
+      last_name: employee.last_name,
+      email: employee.email,
+      gender: employee.gender,
+      designation: employee.designation,
+      salary: employee.salary,
+      department: employee.department
+    });
+  }
+
+   resetForm() {
+    this.employeeForm.reset();
+    this.isUpdating = false;
+    this.selectedEmployeeId = null;
+  }
+}
--- frontend/src/app/services/employee-service.ts
+++ frontend/src/app/services/employee-service.ts
@@ -1,6 +1,102 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { map, Observable } from 'rxjs';
+
+import {GET_EMPLOYEES, GET_EMPLOYEE_BY_DEPARTMENT, GET_EMPLOYEE_BY_ID, ADD_EMPLOYEE, DELETE_EMPLOYEE, UPDATE_EMPLOYEE} from '../graphql/employee.queries'
 
 @Injectable({
   providedIn: 'root',
 })
-export class EmployeeService {}
+export class EmployeeService {
+private readonly apollo = inject(Apollo)
+
+getEmployees(): Observable<any[]> {
+    return this.apollo
+      .watchQuery<any>({ query: GET_EMPLOYEES })
+      .valueChanges.pipe(
+        map((res) => {
+          console.log('Fetched Employees:', res);
+          return res?.data?.employees || [];
+        })
+      );
+  }
+
+getEmployeeById(id: string): Observable<any> {
+    return this.apollo
+      .query<any>({
+        query: GET_EMPLOYEE_BY_ID,
+        variables: { EmployeeId: id },
+      })
+      .pipe(
+        map((res) => {
+          return res.data.employee;
+        })
+      );
+  }
+
+getEmployeeByDepartment(department: string): Observable<any> {
+    return this.apollo
+      .query<any>({
+        query: GET_EMPLOYEE_BY_DEPARTMENT,
+        variables: { EmployeeDepartment: department },
+      })
+      .pipe(
+        map((res) => {
+          return res.data.employee;
+        })
+      );
+  }
+
+   addEmployee(employee: any): Observable<any> {
+    return this.apollo
+      .mutate({
+        mutation: ADD_EMPLOYEE,
+        variables: {
+          first_name: employee.first_name,
+          last_name: employee.last_name,
+          email: employee.email,  
+          gender: employee.gender,
+          designation: employee.designation,
+          salary: employee.salary,
+          department: employee.department,
+        },
+        refetchQueries: [{ query: GET_EMPLOYEES }],
+      })
+      .pipe(
+        map((res: any) => res.data.createEmployee),
+      );
+  }
+
+updateEmployee(id: string, employee: any): Observable<any> {
+    return this.apollo
+      .mutate({
+        mutation: UPDATE_EMPLOYEE,
+        variables: {
+          updateEmployeeId: id,
+          first_name: employee.first_name,
+          last_name: employee.last_name,
+          email: employee.email,  
+          gender: employee.gender,
+          designation: employee.designation,
+          salary: employee.salary,
+          department: employee.department,
+        },
+        refetchQueries: [{ query: GET_EMPLOYEES }],
+      })
+      .pipe(
+        map((res: any) => res.data.updateEmployee)
+      );
+  }
+
+   deleteEmployee(id: string): Observable<any> {
+    return this.apollo
+      .mutate({
+        mutation: DELETE_EMPLOYEE,
+        variables: { deleteStudentId: id },
+        refetchQueries: [{ query: GET_EMPLOYEES }],
+      })
+      .pipe(
+        map((res: any) => res.data.deleteEmployee)
+      );
+  }
+}
